2c0342a68010650bc86466bebddd483b06b9bdd8
benhao <2274507134@qq.com>
2024-04-08 16:24:56 +0800

    del

--- docs/.vitepress/dist/assets/mds_前端_JS_GC.md.7dS8WTH-.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as i,a4 as e}from"./chunks/framework.C6n_gMO8.js";const g=JSON.parse('{"title":"检测方法","description":"","frontmatter":{},"headers":[],"relativePath":"mds/前端/JS/GC.md","filePath":"mds/前端/JS/GC.md"}'),t={name:"mds/前端/JS/GC.md"},n=e("",10),l=[n];function h(p,k,r,o,d,E){return i(),a("div",null,l)}const _=s(t,[["render",h]]);export{g as __pageData,_ as default};
--- docs/about.md
+++ docs/about.md
@@ -1,1 +1,1 @@
-精通  **`js vue nodejs nginx linux java springboot redis mysql`** 等单词拼写
+在你面前的是：精通  **`js vue nodejs nginx linux java springboot redis mysql`** 等单词拼写 ---Tuple
